b2624fbbf4457760e09102f00fcd2410a951336e
Andrey Samaev <andreysamaev@gmail.com>
2022-10-11 14:52:52 +0300

    error handled

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { delay } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, delay, Observable, retry, throwError } from 'rxjs';
+import { ErrorService } from './error.service';
+import { IOrder } from '../models/orders';
 
 @Injectable()
 export class OrdersService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private errorService: ErrorService) {}
 
-  public getAll() {
-    return this.http.get('assets/orders.json');
+  public getAll(): Observable<IOrder[]> {
+    return this.http
+      .get<IOrder[]>('assets/orders.json')
+      .pipe(delay(500), retry(2), catchError(this.errorHandler.bind(this)));
+  }
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
   }
 }
